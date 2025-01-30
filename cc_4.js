8b783398e2c3bded072750cb146f089f25860ac1
Jbarb25 <barboza205@usf.edu>
2025-01-30 17:17:54 -0500

    Task 2 - Sales Report
    Using For Loop and Template Literals for Console.log

--- cc_4.js
+++ cc_4.js
@@ -8,3 +8,15 @@ if (purchaseAmount > 100){
 else {}
 
 console.log(`Final amount after discount: $${purchase_Total}`)
+
+
+//Task 2: For Loop
+let sales = [120, 85, 200, 150, 90]
+let total_sales = 0
+    //Task 2: Sales Report using For Loop
+for (sale in sales){
+    total_sales = sales.reduce((total_sales, sale) => total_sales + sale, 0)}
+
+console.log(`Total Sales: $${total_sales}`)
+
+
