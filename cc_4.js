f5fb98647b1d62ef5be458140e72e5bdb30e814d
Jbarb25 <barboza205@usf.edu>
2025-01-30 17:00:56 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,10 @@
+//Task 1: If Statements
+var purchaseAmount = 150.99
+discount = 0.10
+    //Task 1: Customer Discounts using If Statements
+if (purchaseAmount > 100){
+    purchaseDiscount = purchaseAmount*discount
+    purchase_Total = purchaseAmount - purchaseDiscount}
+else {}
+
+console.log(`Final amount after discount: $${purchase_Total}`)
